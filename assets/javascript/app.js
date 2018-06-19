fe5c8a2cad0f1b9b804c56966551a5bfaee01b89
kurryderp <kurrytran32@gmail.com>
2018-06-19 05:02:38 -0700

    js working, need to style and format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,73 @@
-//personal api key
+// //personal api key
 let APIKEY = 'GIm3UiSv15lSaX4ahu1AiJYPlqSjP6wX';
 
-//structuring query url so that it fits in whatever api i set and dynamically searches based on user input
-// let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=" + APIKEY + "&limit=5";
-
-
+//adding more buttons
 //setting to get the value from search bar when submit is clicked
-$('button:submit').on('click', function () {
-    //might have to push into array and join by "+" because of url formatting
-    let userSearch = $('input:text').val();
+$('#submitbtn').on('click', function () {
+    let $userWord = $('input:text').val();
+    let $userbtn = $('<button>')
 
-    //structuring query url so that it fits in whatever api i set and dynamically searches based on user input
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + "&api_key=" + APIKEY + "&limit=5&rating=g";
+    $userbtn.text($userWord)
+    $userbtn.attr('class', 'jiffy');
+    $userbtn.attr('data-muppet', $userWord);
+    $('#gifTags').append($userbtn);
 
-    console.log(userSearch)
-    //clears the search bar to indicate search is logged
     $('input:text').val("");
 
-    //might end up having to put all of ajax calling into submit button onclick listener because it will activate when the submit button is clicked
+
+});
+
+//event listener for buttons to add gifs to page
+$(document).on('click', '#gifTags .jiffy', function () {
+    $('#gif-holder').empty();
+    let gifTerm = $(this).attr('data-muppet');
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTerm + "&api_key=" + APIKEY + "&limit=5&rating=g";
     $.ajax({
+
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
         //console logs the response from the ajax request so we can pick out what we want from the JSON data object
         console.log(response);
-            let results = response.data
+        let results = response.data
         //for loop to run through the response data array and grabbing specific items
-        for( let i=0; i < results.length; i++ ){
-            //put rating into variable for easier calling
-            let rated = results[i].rating;
-            //setting a variable to make creating a div dynamically easier
-            let $gifDiv = $('<div class="gifs">')
-            //structure where the rating will go
-            let $p = $('<p>').text("Rating: " + rated);
-            //setting the image tag to variable for easier calling
-            let $jif = $('<img>');
-            //giving the image tag attributes for the src image
+        for (let i = 0; i < results.length; i++) {
 
+            //doesnt take results that are rated r or pg13
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                //put rating into variable for easier calling
+                let rated = results[i].rating;
+                //setting a variable to make creating a div dynamically easier
+                let $gifDiv = $('<div class="gifs">')
+                //structure where the rating will go
+                let $p = $('<p>').text("Rating: " + rated);
+                //setting the image tag to variable for easier calling
+                let $jif = $('<img>');
+                //giving the image tag attributes for the src image
+                $jif.attr('src', results[i].images.fixed_height_still.url);
+                $jif.attr('class', 'picture');
+                $jif.attr('data-state', 'still');
+                $jif.attr('data-animate', results[i].images.fixed_height.url);
+                $jif.attr('data-still', results[i].images.fixed_height_still.url);
+                //piecing together the gif div
+                $gifDiv.append($p);
+                $gifDiv.append($jif);
+                $('#gif-holder').prepend($gifDiv);
+            }
         }
-        
     });
 
+});
 
+$(document.body).on('click', '.picture', function () {
+    let state = $(this).attr('data-state');
+    console.log(2);
+    console.log(state);
+    if (state === 'still') {
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state', 'still');
+    }
 });
